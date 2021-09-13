0d961b548313886cdca9ad6ae6286ba96bff59dc
mdjoo0810 <mdjoo0810@gmail.com>
2021-09-13 17:22:04 +0900

    feat: build setting

--- auth-spring-boot-core/src/test/java/com/laonstory/oauth/core/util/JwtTokenProviderTest.java
+++ auth-spring-boot-core/src/test/java/com/laonstory/oauth/core/util/JwtTokenProviderTest.java
@@ -2,16 +2,8 @@ package com.laonstory.oauth.core.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.laonstory.oauth.core.model.TokenModel;
 import org.junit.jupiter.api.Test;
 
 class JwtTokenProviderTest {
-
-  @Test
-  void generateToken() {
-    JwtTokenProvider provider = new JwtTokenProvider(new JwtProperties());
-    TokenModel unique = provider.getTokens("unique");
-    assertEquals(unique.getTokenType(), "Bearer");
-  }
 
 }
